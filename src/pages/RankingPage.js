c22cf20bf075ce25205467d4c46ea33fa098afc7
sylvia1213 <yeayoung1213@naver.com>
2024-02-02 00:34:18 +0900

    랭킹 필터 버튼 추가

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -4,27 +4,39 @@ import RankingList from '../components/RankingList';
 import userIconPink from "../assets/icons/userIcon_pink.png";
 import userIconGreen from "../assets/icons/userIcon_green.png";
 import userIconBrown from "../assets/icons/userIcon_brown.png";
-
+import RankingFilter from '../components/RankingFilter';
 
 const PageContainer = styled.div`
+    width: 100%;
     height: 800px;
-    padding: 150px 500px;
+    padding: 100px;
     background: #132043;
     margin: 0 auto;
 `;
+
+const RankContent = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    width: 800px;
+`
 
 const RankTitle = styled.div`
     color: #FFFFFF;
     font-size: 35px;
     font-weight: bold;
     text-align: center;
-    margin: 0 0 50px 300px;
+    margin: 0 auto;
+    padding: 0 0 0 100px;
+    width: 700px;
 `
 
 const RankWrapper = styled.div`
     background-color: rgba( 255, 255, 255, 0.3 );
-    margin: 0 0 0 300px;
+    margin: 0 30px;
     padding: 10px 0;
+    width: 800px;
+    height: 500px;
     border-radius: 20px;
 `
 
@@ -42,20 +54,22 @@ const Subtitle = styled.div`
 const RankingPage = () => {
     return (
         <PageContainer>
-            <RankTitle>Ranking</RankTitle>
-            <RankWrapper>
-            <Subtitle>
-                <div>Rank</div>
-                <div>Name</div>
-                <div>Score</div>
-            </Subtitle>
-            <RankingList 
-                rank={'1st'}
-                img={userIconBrown} 
-                altText={'user3Icon'} 
-                id={'짱짱'}
-                score={'600'}
-            />
+             <RankTitle>Ranking</RankTitle>
+             <RankContent>
+                <RankingFilter />
+                <RankWrapper>
+                    <Subtitle>
+                        <div>Rank</div>
+                        <div>Name</div>
+                        <div>Score</div>
+                    </Subtitle>
+                <RankingList 
+                    rank={'1st'}
+                    img={userIconBrown} 
+                    altText={'user3Icon'} 
+                    id={'짱짱'}
+                    score={'600'}
+                />
              <RankingList 
                 rank={'2nd'}
                 img={userIconGreen} 
@@ -78,6 +92,7 @@ const RankingPage = () => {
                 score={'270'}
             />
             </RankWrapper>
+            </RankContent>
         </PageContainer>
     );
 };
