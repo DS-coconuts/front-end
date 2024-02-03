4771e7737cb351a9ea80649f8a34803ed227f76c
Soomxn <user email>
2024-02-03 16:21:54 +0900

    Feature: 로그아웃 기능 구현

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import logo_pink from "../assets/icons/logo_pink.png";
@@ -15,7 +15,7 @@ const NavContainer = styled.nav`
 
 const TextLink = styled(Link)`
   text-decoration: none;
-`
+`;
 
 const NavItems = styled.div`
   display: flex;
@@ -26,42 +26,116 @@ const NavItem = styled.p`
   margin: 0 10px;
   color: #132043;
   font-size: 16px;
-  font-family: 'NanumSquareNeo';
+  font-family: "NanumSquareNeo";
 `;
 
 const Navbar = () => {
-  // 로그인 기능 구현 전 임시
-  const isLoggedIn = true;
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [userData, setUserData] = useState({
+    loginId: "",
+    image: userIcon,
+  });
 
-  return(
-  <NavContainer>
-    <NavItems>
-      <TextLink to="/">
-        <img src={logo_pink} alt={'Logo'} style={{ width: 'auto', height: '50px', marginTop: '5px', marginRight: '10px' }} />
-      </TextLink>
-      <TextLink to="/">
-        <NavItem>Typing</NavItem>
-      </TextLink>
-      <TextLink to="/ranking">
-        <NavItem>Ranking</NavItem>
-      </TextLink>
-    </NavItems>
-  <NavItems>
-    {isLoggedIn===true ? (
-        <>
-          <img src={userIcon} alt={'userIcon'} style={{ width: 'auto', height: '25px', marginTop: '5px'}} />
-          <TextLink to="/my">
-            <NavItem>abc</NavItem>
-          </TextLink>
-          <NavItem>Logout</NavItem>
-        </>
-      ) : (
-        <TextLink to="/login">
-          <NavItem>Login</NavItem>
+  // api 연동 후 삭제
+  const mockUserData = {
+    loginId: "abc",
+    password: "1234",
+    image: userIcon,
+  };
+
+  useEffect(() => {
+    // 예시 API 주소 및 로그인 확인
+    const checkLoginStatus = async () => {
+      try {
+        const response = await fetch("/api/users/login", {
+          method: "GET",
+        });
+
+        const data = await response.json();
+
+        if (data.status === 200 && data.code === "SUCCESS_LOGIN") {
+          setIsLoggedIn(true);
+          // 응답에서 loginId와 image를 추출하여 상태를 업데이트합니다.
+          setUserData({
+            loginId: data.data.loginId,
+            image: data.data.image,
+          });
+        }
+      } catch (error) {
+        console.error("로그인 상태 확인 중 오류 발생:", error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []); // useEffect를 한 번만 호출하도록 빈 배열을 전달합니다.
+
+  const handleLogout = async () => {
+    try {
+      const response = await fetch("/api/users/logout", {
+        method: "GET",
+      });
+
+      const data = await response.json();
+
+      if (data.status === 200 && data.code === "SUCCESS_LOGOUT") {
+        // 사용자가 로그아웃되었음을 반영하도록 상태를 업데이트합니다.
+        setIsLoggedIn(false);
+        alert("로그아웃 되었습니다.");
+        window.location.href = `/`;
+      } else {
+        // 에러 케이스를 처리하세요
+        console.error("로그아웃 실패:", data.message);
+      }
+    } catch (error) {
+      console.error("로그아웃 중 오류 발생:", error);
+      // }
+    }
+  };
+
+  return (
+    <NavContainer>
+      <NavItems>
+        <TextLink to="/">
+          <img
+            src={logo_pink}
+            alt={"Logo"}
+            style={{
+              width: "auto",
+              height: "50px",
+              marginTop: "5px",
+              marginRight: "10px",
+            }}
+          />
         </TextLink>
-      )}
-    </NavItems>
-  </NavContainer>    
+        <TextLink to="/">
+          <NavItem>Typing</NavItem>
+        </TextLink>
+        <TextLink to="/ranking">
+          <NavItem>Ranking</NavItem>
+        </TextLink>
+      </NavItems>
+      <NavItems>
+        {isLoggedIn === true ? (
+          <>
+            <img
+              src={userData.image}
+              alt={"userIcon"}
+              style={{ width: "auto", height: "25px", marginTop: "5px" }}
+            />
+            <TextLink to="/my">
+              <NavItem>{userData.loginId}</NavItem>
+            </TextLink>
+            <TextLink to="/">
+              <NavItem onClick={handleLogout}>로그아웃</NavItem>
+            </TextLink>
+          </>
+        ) : (
+          <TextLink to="/login">
+            <NavItem>Login</NavItem>
+          </TextLink>
+        )}
+      </NavItems>
+    </NavContainer>
   );
 };
 
--- src/pages/EditProfilePage.js
+++ src/pages/EditProfilePage.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
-import ProfileImg from "../assets/icons/UserProfileSymbol.svg";
+// import ProfileImg from "../assets/icons/UserProfileSymbol.svg";
 
 const PageContainer = styled.div`
   background-color: #132043;
@@ -8,6 +9,7 @@ const PageContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column; /* 세로 중앙 정렬을 위한 추가 설정 */
+  font-family: "NanumSquareNeo";
 `;
 
 const UserProfileWrapper = styled.div`
@@ -17,6 +19,7 @@ const UserProfileWrapper = styled.div`
   align-items: center;
   margin-top: 50px;
   margin-bottom: 20px;
+  justify-content: center;
 `;
 
 const UserProfileImg = styled.img`
@@ -25,7 +28,7 @@ const UserProfileImg = styled.img`
   margin-bottom: 20px; /* 로고와 텍스트 사이에 여백을 주기 위한 추가 설정 */
 `;
 
-const UserProfileChangeButton = styled.div`
+const UserProfileChangeButton = styled.label`
   width: 220px;
   height: 40px;
   border-radius: 15px;
@@ -33,12 +36,17 @@ const UserProfileChangeButton = styled.div`
   color: #132043;
   text-align: center;
   font-size: 20px;
-  font-weight: 600;
   display: flex;
   align-items: center; /* 세로 방향에서 중앙 정렬을 위해 추가 */
   justify-content: center; /* 가로 방향에서 중앙 정렬을 위해 추가 */
   cursor: pointer;
+  font-family: "bitbit";
 `;
+
+const HiddenFileInput = styled.input`
+  display: none;
+`;
+
 const UserInformationWrapper = styled.div`
   width: 400px;
   color: #132043;
@@ -104,32 +112,121 @@ const EditButton = styled.div`
   background: #f1b4bb;
   color: #132043;
   text-align: center;
-  font-size: 20px;
-  font-weight: 800;
+  font-size: 25px;
   display: flex;
   align-items: center; /* 세로 방향에서 중앙 정렬을 위해 추가 */
   justify-content: center; /* 가로 방향에서 중앙 정렬을 위해 추가 */
   cursor: pointer;
+  font-family: "bitbit";
 `;
 
 const EditProfilePage = () => {
+  const [userIntroduction, setUserIntroduction] = useState("");
+  const [userGoalcpm, setUserGoalcpm] = useState("");
+  const [userImage, setUserImage] = useState();
+
+  const userId = localStorage.getItem("userId");
+
+  /*api 연동 후에 삭제*/
+  const mockUserData = {
+    introduction: "youna입니다:)",
+    goalcpm: "600",
+    image:
+      "https://i.namu.wiki/i/XtJ0E7b6qoVPX394BSWRRvtgqbklV6a21OzwCfevS0QEP6mSjgn3aUvnJwplZk0M7-UZV6rpUQ_ufg8xl77Bcw.webp",
+  };
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  // const response = await axios.get(`/api/users/${userId}`);
+  // const userData = response.data.data;
+  // setUserIntroduction(userData.introduction);
+  // setUserGoalcpm(userData.goalcpm);
+  // setUserImage(userData.image);
+  //   } catch (error) {
+  //     console.error("Error fetching user data:", error);
+  //   }
+  // };
+  //   fetchData();
+  // }, [userId]);
+
+  /*api 연동 후에 삭제*/
+  useEffect(() => {
+    setUserIntroduction(mockUserData.introduction);
+    setUserGoalcpm(mockUserData.goalcpm);
+    setUserImage(mockUserData.image);
+  }, [mockUserData]);
+
+  const handleEdit = () => {
+    // 수정된 정보를 서버에 전송
+    const formData = new FormData();
+    formData.append("introduction", userIntroduction);
+    formData.append("goalcpm", userGoalcpm);
+    formData.append("image", userImage);
+
+    axios
+      .patch(`/api/users/${userId}`, formData)
+      .then((response) => {
+        const updatedUserData = response.data.data;
+        setUserIntroduction(updatedUserData.introduction);
+        setUserGoalcpm(updatedUserData.goalcpm);
+        setUserImage(updatedUserData.image);
+        console.log(updatedUserData);
+        alert("수정 완료!");
+      })
+      .catch((error) => {
+        console.error("Error editing user data:", error);
+      });
+    console.log(formData);
+    // window.location.href = `/my`;
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        setUserImage(e.target.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <PageContainer>
-      <UserProfileWrapper>
-        <UserProfileImg src={ProfileImg} alt="ProfileImg" />
-        <UserProfileChangeButton>프로필 사진 변경</UserProfileChangeButton>
+      <UserProfileWrapper onSubmit={handleImageChange}>
+        <UserProfileImg src={userImage} alt="ProfileImg" />
+        <UserProfileChangeButton>
+          프로필 사진 변경
+          <HiddenFileInput
+            type="file"
+            accept="image/*"
+            onChange={(e) => handleImageChange(e)}
+          />
+        </UserProfileChangeButton>
       </UserProfileWrapper>
       <UserInformationWrapper>
         <UserIntroWrapper>
           <UserIntroLabel>한 줄 소개</UserIntroLabel>
-          <UserIntroInput type="text" placeholder="" />
+          <UserIntroInput
+            type="text"
+            placeholder={userIntroduction}
+            onChange={(e) => setUserIntroduction(e.target.value)}
+          />
         </UserIntroWrapper>
         <UserGoalWrapper>
           <UserGoalLabel>목표 타수</UserGoalLabel>
-          <UserGoalInput type="number" placeholder="" />
+          <UserGoalInput
+            type="text"
+            placeholder={userGoalcpm}
+            onChange={(e) => setUserGoalcpm(e.target.value)}
+          />
         </UserGoalWrapper>
       </UserInformationWrapper>
-      <EditButton>수정</EditButton>
+      <EditButton onClick={handleEdit}>수정</EditButton>
     </PageContainer>
   );
 };
